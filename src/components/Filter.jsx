0307b3f70c087d20448f662178332cb3df283e60
Bra <69542776+Med-hedi-bra@users.noreply.github.com>
2021-10-31 21:58:34 +0100

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -38,7 +38,7 @@ const Filter = () => {
             const host1 = window.location.hostname
             // let url =protocol+"//"+host1+":8080/apiStuff"
             let url = "https://backenddarkre.herokuapp.com/apiStuff"
-              const response = await axios.get("https://backenddarkre.herokuapp.com/apiStuff/617d765466aa19afb3459e72");
+              const response = await axios.get("https://backenddarkre.herokuapp.com/apiStu");
              console.log(response)
               
               if(response.data.data.length==0){
