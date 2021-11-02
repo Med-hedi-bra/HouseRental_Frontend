cc11d191182e729218212931146a28482045fc48
Bra <69542776+Med-hedi-bra@users.noreply.github.com>
2021-11-02 14:42:00 +0100

    Update NouvelleAnnoce.jsx

--- src/pages/NouvelleAnnoce.jsx
+++ src/pages/NouvelleAnnoce.jsx
@@ -105,9 +105,9 @@ formData.append("img5",Image["img5"])
 
 
 try {
-   const resp = await axios.post(url,formData,headers:{
+   const resp = await axios.post(url,formData,{headers:{
                                  'Access-Control-Allow-Origin':'*'
-                                 })
+                                 }})
     console.log(resp)
     setMessage(true)
     setAfficheMessage("Votre Maison a été Bien ajouté")
